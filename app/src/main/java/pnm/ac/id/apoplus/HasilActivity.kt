b87debc509bd79bc4f1c09221f3be6a7695d72f8
Zihan30 <cahyaam.zi25@gmail.com>
2024-06-03 08:51:34 +0700

    comit

--- app/src/main/java/pnm/ac/id/apoplus/HasilActivity.kt
+++ app/src/main/java/pnm/ac/id/apoplus/HasilActivity.kt
@@ -1,6 +1,9 @@
 package pnm.ac.id.apoplus
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.ComponentActivity
 
@@ -27,20 +30,23 @@ class HasilActivity : ComponentActivity() {
         val iliadinTotal = iliadinQuantity * iliadinPrice
 
         // Find TextViews
-        val tvDexteemQuantity = findViewById<TextView>(R.id.jumlah_pesanan_value_text_view)
-        val tvPonstanQuantity = findViewById<TextView>(R.id.harga_obat_value_text_view)
-        val tvClinovirQuantity = findViewById<TextView>(R.id.harga_obat_value_text_view)
-        val tvIliadinQuantity = findViewById<TextView>(R.id.harga_obat_value_text_view)
         val tvTotalBayar = findViewById<TextView>(R.id.total_bayar_value_text_view)
-
-        // Set text to TextViews
-        tvDexteemQuantity.text = "$dexteemQuantity"
-        tvPonstanQuantity.text = "$ponstanQuantity"
-        tvClinovirQuantity.text = "$clinovirQuantity"
-        tvIliadinQuantity.text = "$iliadinQuantity"
 
         // Calculate and set total payment
         val totalBayar = dexteemTotal + ponstanTotal + clinovirTotal + iliadinTotal
         tvTotalBayar.text = "Rp.$totalBayar"
+
+        // Find the confirmation button
+        val konfirmasiButton = findViewById<Button>(R.id.konfirmasi_button)
+
+        // Set click listener for the confirmation button
+        konfirmasiButton.setOnClickListener {
+            // Create an intent to navigate back to the main menu
+            val intent = Intent(this, MainActivity::class.java)
+            // Start the MainActivity
+            startActivity(intent)
+            // Finish this activity
+            finish()
+        }
     }
 }
